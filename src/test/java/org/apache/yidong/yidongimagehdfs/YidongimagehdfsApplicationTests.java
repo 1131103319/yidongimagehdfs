ace62fade7ceabfb846de697dbae41feea36c857
隐藏玩家 <1131103319@qq.com>
2025-09-25 20:23:13 +0800

    super循环

--- src/test/java/org/apache/yidong/yidongimagehdfs/YidongimagehdfsApplicationTests.java
+++ src/test/java/org/apache/yidong/yidongimagehdfs/YidongimagehdfsApplicationTests.java
@@ -1,11 +1,9 @@
 package org.apache.yidong.yidongimagehdfs;
 
-import org.apache.yidong.yidongimagehdfs.model.FileStatistics;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.time.LocalDate;
-import java.util.ArrayList;
+import java.io.File;
 
 @SpringBootTest
 class YidongimagehdfsApplicationTests {
@@ -15,11 +13,13 @@ class YidongimagehdfsApplicationTests {
 //        LocalDate parse = LocalDate.parse("2025-10-11", FileStatistics.dateTimeFormatter1);
 //        System.out.println(parse.toString());
 //        System.out.println(File.separator);
-        ArrayList<String> timearray = new ArrayList<>();
-        for(int i=0;i<3;i++){
-            timearray.add(LocalDate.now().minusDays(i).format(FileStatistics.dateTimeFormatter1));
-            timearray.add(LocalDate.now().minusDays(i).format(FileStatistics.dateTimeFormatter2));
-        }
-        System.out.println(timearray);
+//        ArrayList<String> timearray = new ArrayList<>();
+//        for(int i=0;i<3;i++){
+//            timearray.add(LocalDate.now().minusDays(i).format(FileStatistics.dateTimeFormatter1));
+//            timearray.add(LocalDate.now().minusDays(i).format(FileStatistics.dateTimeFormatter2));
+//        }
+//        System.out.println(timearray);
+        File tmpFile = new File("/tmp/hello1");
+        System.out.println(tmpFile.getAbsolutePath());
     }
 }
